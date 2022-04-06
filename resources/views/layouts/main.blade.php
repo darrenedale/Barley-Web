6a29005991d0eaa7dbd9e79edc2b225d1af30359
Darren Edale <darren@equituk.net>
2022-04-06 10:28:42 +0100

    Added home page
    - home page can create barcode images
    - very rough styling just to make it vaguely presentable

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -9,27 +9,9 @@
 <header>
     <h1>Barley</h1>
     @yield("header-content")
-    @if (\Illuminate\Support\Facades\Auth::check())
-        <div class="account-avatar">
-            {{ \Illuminate\Support\Facades\Auth::user()->username }}
-        </div>
-    @else
-        {{-- TODO extract this to an include/component --}}
-        <div class="login-form">
-            <form>
-                @csrf
-                @error("username")
-                <span class="barley-error">{{ $errors->first("username") }}</span>
-                @enderror
-                <input @error("username") class="barley-error-field" @enderror type="text" name="username" placeholder="Username or email..." />
-                <input type="password" name="password" placeholder="Password..." />
-                <button type="submit" formaction="/login" formmethod="POST">Log in</button>
-            </form>
-        </div>
-    @endif
 </header>
 <section id="main-content" class="main-content">
-    @yield("main-content")
+@yield("main-content")
 </section>
 <footer>
 </footer>
